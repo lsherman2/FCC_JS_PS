365445a067c26d996b6f53f2ebec7632f8c7747d
lsherman2 <logansherman1@gmail.com>
2024-02-08 21:03:50 -0600

    Added variables and first function.

---
+++ script.js
@@ -0,0 +1,18 @@
+const url = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/";
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const pokeName = document.getElementById("pokemon-name");
+const pokeId = document.getElementById("pokemon-id");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const types = document.getElementById("types");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAtk = document.getElementById("special-attack");
+const specialDef = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+
+const inputValidation = () => {
+
+};
